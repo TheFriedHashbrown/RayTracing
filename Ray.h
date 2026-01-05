604924a6bd35a004dd701a4c87251e34b7492c1f
TheFriedHashbrown <amygreat0@gmail.com>
2026-01-06 01:27:37 +0530

    Added the Ray class

---
+++ Ray.h
@@ -0,0 +1,35 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include<stdio.h>
+#include "Vec3.h"
+
+class Ray{
+    public:
+    vec3 orig;
+    vec3 dir;
+
+    Ray () {
+
+    }
+
+    Ray (vec3 orig, vec3 dir){
+        this -> orig = orig;
+        this -> dir = dir;
+    }
+
+    vec3 origin() const{
+        return orig;
+    }
+
+    vec3 direction() const{
+        return dir;
+    }
+
+    vec3 at (double t) const{
+        vec3 ans = orig + t*dir;
+        return ans;
+    }
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<iostream>
 #include<fstream>
-#include "vec3.h"
-#include "color.h"
+#include "Vec3.h"
+#include "Color.h"
+#include "Ray.h"
+
+vec3 ray_color();
 
 int main(){
     //Setting the canvas size
@@ -13,15 +16,31 @@ int main(){
     testFile << "P3\n" << img_width << " " << img_height << "\n255\n";
     //P3 identifes that it is a ppm file, then the width and height and 255 is the maximum colour value of a pixel in the map
 
+
+
     for (int i = 0; i < img_height; ++i){
         for (int j = 0; j < img_width; ++j){
-            int r = double(j) / (img_width - 1);
-            int g = double(i) / (img_height - 1);
-            int b = 0;
+            double r = double(j) / (img_width - 1);
+            double g = double(i) / (img_height - 1);
+            double b = 0;
 
             vec3 pixel_color (r, g, b);
             write_color(testFile, pixel_color);
         }
     }
     testFile.close();
+}
+
+//Writing a function for the background
+vec3 ray_color (const Ray& r){
+    //Normalizes the vector to have a y value from -1 to 1
+    vec3 unit = unit_vector(r.direction());
+    
+    //Scales it from 0 to 1 so that we can use t as percentage
+    double t = 0.5 * (unit.y() + 1);
+
+    //Creating a gradient from white to blue for the background
+    vec3 start_val = vec3(1.0, 1.0, 1.0);
+    vec3 end_val = vec3(0.5, 0.7, 1.0);
+    return ((1.0 - t) * start_val) + (t * end_val);
 }
